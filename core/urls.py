f8c2bd336084e9673b9a31c1964b1eb440e0d958
suman dahal <me.suman11@gmail.com>
2025-01-05 13:59:20 +1100

    url cleaned

--- core/urls.py
+++ core/urls.py
@@ -1,21 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path,include
-from rest_framework import permissions
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from django.conf.urls.static import static
-from django.conf import settings
+
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls')),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
+    path('', include('users.urls')), 
 ]
-urlpatterns += [
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,19 @@
-from django.urls import path
+from django.urls import path,include
+from rest_framework import permissions
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),  
 ]
+urlpatterns += [
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
